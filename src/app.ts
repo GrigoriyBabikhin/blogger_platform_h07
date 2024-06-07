10d45241dc1a4a7c3d9981ca7fe416baf1a30f7f
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-06-07 19:13:35 +0500

    post запрос для blogs.

--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,18 @@ app.get(SETTINGS.PATH.BLOGS, (
     res: Response<BlogsDbType[]>) => {
     res.status(200).json((db.blogs))
 })
+
+app.post(SETTINGS.PATH.BLOGS, (
+    req: Request,
+    res: Response) => {
+    const {name, description, websiteUrl} = req.body
+    let newBlogs: BlogsDbType = {
+        id: Date.now() + Math.random().toString(),
+        name,
+        description,
+        websiteUrl
+    }
+    db.blogs.push(newBlogs)
+    res.status(201).json(newBlogs)
+})
+
--- src/db/db.ts
+++ src/db/db.ts
@@ -4,17 +4,18 @@ export const db: DbType = {
     blogs: [
         {
             "id": "string",
-            "name": "string",
-            "description": "string",
-            "websiteUrl": "string"
+            "name": "string", //maxLength: 15
+            "description": "string", //maxLength: 500
+            "websiteUrl": "string" //maxLength: 100 || regex: ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
+
         }
     ],
     posts: [
         {
             "id": "string",
-            "title": "string",
-            "shortDescription": "string",
-            "content": "string",
+            "title": "string", //maxLength: 30
+            "shortDescription": "string", //maxLength: 100
+            "content": "string", //maxLength: 1000
             "blogId": "string",
             "blogName": "string"
         }
