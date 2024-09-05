91e24d1ee50fe378d4af5504f9b06eabff885864
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-05 23:10:26 +0500

    Добавлена типизация для обработчика ошибок.

--- src/global-middiewares/inputCheckErrorsMiddleware.ts
+++ src/global-middiewares/inputCheckErrorsMiddleware.ts
@@ -1,19 +1,42 @@
 import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
+import {APIErrorResult, FieldError} from "../input-output-types/output-errors-type";
 
 export const inputCheckErrorsMiddleware = (
     req: Request,
     res: Response,
     next: NextFunction) => {
-    //array({onlyFirstError: true})}) вернет только первую ошибку.
-    const e = validationResult(req);
-    const errors = e.array({onlyFirstError: true})
-
-    if (errors.length) {
-        res.status(400).json({errorsMessages: errors.map(i => i.msg)})
-        return
+    try {
+        const e = validationResult(req);
+        const errors = e.array({onlyFirstError: true});
+        if (errors.length) {
+            const formattedErrors: FieldError[] =  errors.map(i => i.msg)
+            const errorResponse: APIErrorResult = {
+                errorsMessages: formattedErrors.length ? formattedErrors : null
+            }
+            return res.status(400).json(errorResponse)
+        }
+      return  next()
+    } catch
+        (e) {
+        const errorResponse: APIErrorResult = {
+            errorsMessages: [
+                {
+                    message: 'server error',
+                    field: null,
+                }
+            ]
+        }
+       return  res.status(500).json(errorResponse)
     }
-    next()
 }
 
 //Должно вернуть: { errorsMessages: [{ message: Any<String>, field: "websiteUrl" }, { message: Any<String>, field: "name" }] }
+//array({onlyFirstError: true})}) вернет только первую ошибку.
+
+
+// if (errors.length) {
+//     res.status(400).json({errorsMessages: errors.map(i => i.msg)})
+//     return
+// }
+// next()
--- src/input-output-types/output-errors-type.ts
+++ src/input-output-types/output-errors-type.ts
@@ -1,10 +1,16 @@
 import {BlogInputModel} from "./blogs-types";
 import {PostInputModel} from "./post-types";
 
-
+//Можно передать только те ключи которые описаны в модели.
 export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel
-// const f: FieldsType = 'some' // error
 
-export type OutputErrorsType = {
-    errorsMessages: {message: string, field: FieldNamesType}[]
+ export type OutputErrorsType = {
+     errorsMessages: {message: string, field: FieldNamesType}[]
+ }
+export type APIErrorResult = {
+    errorsMessages: Array<FieldError> | null
+}
+export type FieldError = {
+    message: string | null
+    field: FieldNamesType | null
 }
