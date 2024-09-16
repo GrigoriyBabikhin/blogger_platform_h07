ebe4023f4879959210980fd092c0398dad0994f2
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-16 22:56:58 +0500

    Добавил blogsIdValidation.

--- src/features/blogs/blogs-router.ts
+++ src/features/blogs/blogs-router.ts
@@ -1,17 +1,17 @@
 import {Router} from "express";
-import {blogsInputValidations} from "./blogsInputValidations";
+import {idValidation, blogsBodyValidations, blogsIdValidation} from "./blogsBodyValidations";
 import {adminAuthentication} from "../../global-middiewares/adminAuthentication";
-import {blogController} from "./blogController";
-
+import {blogsController} from "./blogsController";
+import {inputCheckErrorsMiddleware} from "../../global-middiewares/inputCheckErrorsMiddleware";
 
 
 export const blogsRouter = Router({})
 
-blogsRouter.get('/', blogController.getBlogsController)
-blogsRouter.post('/', adminAuthentication, blogsInputValidations(), blogController.createBlogController)
-blogsRouter.get('/:blogId', blogController.findBlogController)
-blogsRouter.put('/:blogId', adminAuthentication, blogsInputValidations(), blogController.putBlogController)
-blogsRouter.delete('/:blogId', adminAuthentication, blogController.delBlogController)
+blogsRouter.get('/', blogsController.getAllBlogs)
+blogsRouter.post('/', adminAuthentication, ...blogsBodyValidations(), blogsController.createBlog)
+blogsRouter.get('/:blogId', ...blogsIdValidation(), blogsController.getBlogById)
+blogsRouter.put('/:blogId', adminAuthentication,...blogsIdValidation(), ...blogsBodyValidations(), blogsController.updateBlog)
+blogsRouter.delete('/:blogId', adminAuthentication,...blogsIdValidation(), blogsController.deleteBlog)
 
 
 
