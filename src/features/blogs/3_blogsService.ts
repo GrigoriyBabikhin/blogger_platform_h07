4d740df8d959b4769b923f383f24f399bf020d30
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-23 21:35:18 +0500

    feat: добавлен метод для поиска блога по ID
    - Добавлен метод findBlogId в blogsService для поиска блога по ID

--- src/features/blogs/3_blogsService.ts
+++ src/features/blogs/3_blogsService.ts
@@ -1,8 +1,15 @@
 import {BlogInputModel} from "../../input-output-types/blogs-types";
 import {blogsMongoRepository} from "./repository/blogsMongoRepository";
+import {BlogsDbType} from "./blogs-type";
+import {WithId} from "mongodb";
 
 
 export const blogsService = {
+
+    async findBlogId(blogId: string): Promise<WithId<BlogsDbType> | null> {
+      return await blogsMongoRepository.findBlogId(blogId)
+    },
+
     async createBlog(blogInput: BlogInputModel): Promise<string | null> {
         return await blogsMongoRepository.createBlog(blogInput)
     },
