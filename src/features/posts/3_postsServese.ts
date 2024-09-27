86517461835d51655877e1cb1dbe53eb90d6f220
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-27 16:10:44 +0500

    feat: обновлен сервис постов - Добавлен метод createPostByBlogId для создания поста по ID блога - Обновлен метод createPost для добавления новых полей (createdAt, blogName) - Переименован метод deleteALL в deleteALLPosts

--- src/features/posts/3_postsServese.ts
+++ src/features/posts/3_postsServese.ts
@@ -1,9 +1,35 @@
-import {PostInputModel} from "../../input-output-types/post-types";
+import {PostInputByBlogModel, PostInputModel} from "../../input-output-types/post-types";
 import {postsMongoRepository} from "./repsitory/postsMongoRepository";
+import {blogsMongoRepository} from "../blogs/repository/blogsMongoRepository";
 
 export const postsService = {
-    async createPost(postinput: PostInputModel): Promise<string | null> {
-        return await postsMongoRepository.createPost(postinput)
+
+    async createPost(postInput: PostInputModel): Promise<string | null> {
+        const blog = await blogsMongoRepository.findBlogById(postInput.blogId)
+        if(!blog) return null
+        const newPost = {
+            title: postInput.title,
+            shortDescription: postInput.shortDescription,
+            content: postInput.content,
+            blogId: postInput.blogId,
+            blogName: blog.name,
+            createdAt: new Date().toISOString(),
+        }
+        return await postsMongoRepository.createPost(newPost)
+    },
+
+    async createPostByBlogId(blogId: string, postInput: PostInputByBlogModel): Promise<string | null>{
+        const blog = await blogsMongoRepository.findBlogById(blogId)
+        if(!blog) return null
+        const newPostByBlog = {
+            title: postInput.title,
+            shortDescription: postInput.shortDescription,
+            content: postInput.content,
+            blogId: blogId,
+            blogName: blog.name,
+            createdAt: new Date().toISOString(),
+        }
+        return postsMongoRepository.createPost(newPostByBlog)
     },
 
     async updatePost(postId: string, post: PostInputModel): Promise<boolean> {
@@ -14,8 +40,8 @@ export const postsService = {
         return await postsMongoRepository.deletePost(postId)
     },
 
-    async deleteALL(): Promise<boolean> {
-        return await postsMongoRepository.deleteALL()
+    async deleteALLPosts(): Promise<boolean> {
+        return await postsMongoRepository.deleteALLPosts()
     },
 
 }
