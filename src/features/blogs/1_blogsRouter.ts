3025888c03a0f43df167fb6696b7239807d0b273
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-27 16:01:00 +0500

    feat: добавлен маршрут для создания поста по ID блога - Добавлен новый маршрут POST /:blogId/posts - Добавлена аутентификация администратора для нового маршрута - Добавлена валидация ID блога и ввода поста для нового маршрута

--- src/features/blogs/1_blogsRouter.ts
+++ src/features/blogs/1_blogsRouter.ts
@@ -2,12 +2,14 @@ import {Router} from "express";
 import {blogsInputValidations, blogIdParamValidations} from "./blogsInputValidations";
 import {adminAuthentication} from "../../global-middiewares/adminAuthentication";
 import {blogsController} from "./2_blogsController";
+import {PostInputByBlogValidations} from "../posts/postInputValidations";
 
 export const blogsRouter = Router({})
 
 blogsRouter.get('/', blogsController.getAllBlogs)
 blogsRouter.post('/', adminAuthentication, ...blogsInputValidations(), blogsController.createBlog)
 blogsRouter.get('/:blogId/posts',...blogIdParamValidations(), blogsController.getPostsByBlogId)
+blogsRouter.post('/:blogId/posts',adminAuthentication, ...blogIdParamValidations(),...PostInputByBlogValidations(), blogsController.createPostByBlogId)
 blogsRouter.get('/:blogId', ...blogIdParamValidations(), blogsController.getBlogById)
 blogsRouter.put('/:blogId', adminAuthentication,...blogIdParamValidations(), ...blogsInputValidations(), blogsController.updateBlog)
 blogsRouter.delete('/:blogId', adminAuthentication,...blogIdParamValidations(), blogsController.deleteBlog)
