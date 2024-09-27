dfe4d810b8ea212f10df463b9eaee894f8de008f
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-09-27 17:03:57 +0500

    refactor: удалил неиспользуемый тип OutputErrorsType.

--- src/input-output-types/output-errors-type.ts
+++ src/input-output-types/output-errors-type.ts
@@ -1,15 +1,12 @@
 import {BlogInputModel} from "./blogs-types";
-import {PostInputModel} from "./post-types";
+import {PostInputByBlogModel, PostInputModel} from "./post-types";
 
-//Можно передать только те ключи которые описаны в модели.
-export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel
+export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof PostInputByBlogModel
 
- export type OutputErrorsType = {
-     errorsMessages: {message: string, field: FieldNamesType}[]
- }
 export type APIErrorResult = {
     errorsMessages: Array<FieldError> | null
 }
+
 export type FieldError = {
     message: string | null
     field: FieldNamesType | null
