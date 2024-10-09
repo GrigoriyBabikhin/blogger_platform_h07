406378570ed7323b488ecb615915c8bce9ac2607
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-10-09 22:45:04 +0500

    refactor: тесты для blogs

--- src/features/blogs/1_blogsRouter.ts
+++ src/features/blogs/1_blogsRouter.ts
@@ -8,11 +8,11 @@ export const blogsRouter = Router({})
 
 blogsRouter.get('/', blogsController.getAllBlogs)
 blogsRouter.post('/', adminAuthentication, ...blogsInputValidations(), blogsController.createBlog)
-blogsRouter.get('/:blogId/posts',...blogIdParamValidations(), blogsController.getPostsByBlogId)
-blogsRouter.post('/:blogId/posts',adminAuthentication, ...blogIdParamValidations(),...PostInputByBlogValidations(), blogsController.createPostByBlogId)
+blogsRouter.get('/:blogId/posts', ...blogIdParamValidations(), blogsController.getPostsByBlogId)
+blogsRouter.post('/:blogId/posts', adminAuthentication, ...blogIdParamValidations(), ...PostInputByBlogValidations(), blogsController.createPostByBlogId)
 blogsRouter.get('/:blogId', ...blogIdParamValidations(), blogsController.getBlogById)
-blogsRouter.put('/:blogId', adminAuthentication,...blogIdParamValidations(), ...blogsInputValidations(), blogsController.updateBlog)
-blogsRouter.delete('/:blogId', adminAuthentication,...blogIdParamValidations(), blogsController.deleteBlog)
+blogsRouter.put('/:blogId', adminAuthentication, ...blogIdParamValidations(), ...blogsInputValidations(), blogsController.updateBlog)
+blogsRouter.delete('/:blogId', adminAuthentication, ...blogIdParamValidations(), blogsController.deleteBlog)
 
 
 
