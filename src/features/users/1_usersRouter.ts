b9ecfaca6dcddae07e3b456bbe59c03f73bde7ad
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-10-27 10:18:51 +0500

    features: usersRouter

---
+++ src/features/users/1_usersRouter.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+import {usersController} from "./2_usersControiller";
+import {adminAuthentication} from "../../utilities/Middleware/adminAuthentication";
+import {userBodyValidations} from "./usersInputValidations";
+
+export const usersRouter = Router({})
+
+usersRouter.get('/', usersController.getAllUsers)
+usersRouter.post('/', adminAuthentication, ...userBodyValidations(), usersController.createUser)
