29743c31a6f766aa36939613f2f9f19b8237d477
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-10-27 10:19:15 +0500

    features: usersController

---
+++ src/features/users/2_usersControiller.ts
@@ -0,0 +1,36 @@
+import {Request, Response} from "express";
+import {UserInputModel} from "./types/userInputModel";
+import {UserViewModel} from "./types/userViewModel";
+import {usersService} from "./3_usersService";
+import {usersMongoQueryRepository} from "./repository/usersMongoQueryRepository";
+import {ResultStatus} from "../../utilities/resultError/resultStatus";
+import {ErrorMessage} from "../../utilities/resultError/resultType";
+import {Paginator} from "../../utilities/paginationAndSorting/paginator-type";
+import {SortingQueryField} from "../../utilities/paginationAndSorting/paginationAndSorting";
+
+export const usersController = {
+    async getAllUsers(
+        req: Request<any, any, any, SortingQueryField>,
+        res: Response<Paginator<UserViewModel[]>>
+    ){
+        const users = await usersMongoQueryRepository.getAll(req.query)
+        return res.status(200).json(users)
+    },
+
+    async createUser(
+        req: Request<any, any, UserInputModel>,
+        res: Response<UserViewModel | ErrorMessage>
+    ) {
+        const {login, email, password} = req.body
+        const userId = await usersService.createUser({login, email, password})
+
+        const {status, data, errorsMessages} = userId
+        if (status === ResultStatus.BadRequest) {
+            res.status(400).json(errorsMessages)
+        }
+        console.log('data ', data )
+        const mapUser = await usersMongoQueryRepository.findUserId(data!)
+        console.log('mapUser ', mapUser)
+        return res.status(201).json(mapUser!)
+    },
+}
