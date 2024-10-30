78d8fbdc7312cc4c3afd7a5053cb8a2eb651e450
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-10-31 01:49:22 +0500

    refactor: зачистка DB

--- src/features/testing/delTestingController.ts
+++ src/features/testing/delTestingController.ts
@@ -1,7 +1,6 @@
 import {Request, Response} from "express";
 import {blogsService} from "../blogs/3_blogsService";
 import {postsService} from "../posts/3_postsServese";
-import {usersService} from "../users/3_usersService";
 import {usersMongoRepository} from "../users/repository/usersMongoRepository";
 
 export const delTestingController = async (req: Request, res: Response) => {
