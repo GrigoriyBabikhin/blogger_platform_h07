8987854a4eaf7852b6c299d1e630e2eb2c4fbe13
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-14 16:33:12 +0500

    feat: расширил тип для req.

---
+++ src/utilities/input-output-types/index.d.ts
@@ -0,0 +1,14 @@
+import {IdType} from "./id";
+
+
+declare global {
+    declare namespace Express {
+        export interface Request {
+            user: IdType | undefined
+        }
+    }
+}
+
+
+
+
