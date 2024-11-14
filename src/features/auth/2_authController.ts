6072487f7b4d7b52fdb65a984536feb006565ae3
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-14 16:32:05 +0500

    feat: authRouter.get - показывает данные юзера при предоставлении JWT.

--- src/features/auth/2_authController.ts
+++ src/features/auth/2_authController.ts
@@ -1,9 +1,10 @@
-import {LoginInputModel} from "./types";
+import {LoginInputModel, MeViewModel} from "./authModel";
 import {Request, Response} from "express";
 import {authService} from "./authService";
 import {ResultStatus} from "../../utilities/resultError/resultStatus";
 import {LoginSuccessViewModel} from "../../utilities/jwtService/jwtModel";
 import {usersMongoQueryRepository} from "../users/repository/usersMongoQueryRepository";
+import {IdType} from "../../utilities/input-output-types/id";
 
 export const authController = {
     async authUser(
@@ -14,7 +15,7 @@ export const authController = {
         const accessToken = await authService.loginUser({loginOrEmail, password})
 
         const {status, data} = accessToken
-        if(status === ResultStatus.Unauthorized) {
+        if (status === ResultStatus.Unauthorized) {
             res.status(401).json()
             return
         }
@@ -22,4 +23,18 @@ export const authController = {
         const accessTokenDTO = await usersMongoQueryRepository.accessTokenDTO(data!)
         return res.status(200).json(accessTokenDTO)
     },
+
+    async getUserMe(
+        req: Request<any, any, any, IdType>,
+        res: Response<MeViewModel>
+    ) {
+        const userId = req?.user?.id as string
+
+        if(!userId) return res.sendStatus(401)
+        const me = await usersMongoQueryRepository.findUserIdDtoMe(userId)
+
+        return res.status(200).send(me!)
+
+
+    }
 }
