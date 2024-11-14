70fd37ee6dcfe946d041baa3c78408db2d11e9e3
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-14 16:32:00 +0500

    feat: authRouter.get - показывает данные юзера при предоставлении JWT.

--- src/features/auth/1_authRouter.ts
+++ src/features/auth/1_authRouter.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import {authController} from "./2_authController";
 import {authInputValidation} from "./authInputValidation";
+import {accessTokenGuard} from "./guards/accessTokenGuard";
 
 
 export const authRouter = Router({})
 
+authRouter.get('/me', accessTokenGuard, authController.getUserMe)
 authRouter.post('/login',...authInputValidation(), authController.authUser)
-authRouter.get('/me',)
