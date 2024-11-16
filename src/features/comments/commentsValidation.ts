5f812635fd39aaef20113a7060212d51e210068b
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-16 20:13:59 +0500

    comments

---
+++ src/features/comments/commentsValidation.ts
@@ -0,0 +1,11 @@
+import {body} from "express-validator";
+import {inputCheckErrorsMid} from "../../utilities/inputCheckErrors/inputCheckErrorsMid";
+
+export const commentsInputValidation = () => [
+    commentsBodyValidation, inputCheckErrorsMid
+]
+
+export const commentsBodyValidation = body('content')
+    .isString().withMessage({message: 'There should be a string', field: 'content'})
+    .trim().isLength({min: 20, max: 300})
+    .withMessage({message: 'string of 20 to 300 symbol.', field: 'title'})
