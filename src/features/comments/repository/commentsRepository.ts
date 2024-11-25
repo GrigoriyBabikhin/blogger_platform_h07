23196f37e6b92bc808e8e5a9c493a8b00d190791
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-25 20:06:20 +0500

    Update existing comment by id with InputModel

--- src/features/comments/repository/commentsRepository.ts
+++ src/features/comments/repository/commentsRepository.ts
@@ -1,10 +1,27 @@
-import {CommentDBType} from "../commentModel";
+import {CommentDbModel, CommentInputModel} from "../commentModel";
 import {commentCollection} from "../../../db/mongo-db";
+import {ObjectId} from "mongodb";
 
 export const commentsRepository = {
-    async create (newComment: CommentDBType) {
+    async create(newComment: CommentDbModel) {
         const comment = await commentCollection.insertOne(newComment)
         return comment.insertedId ? comment.insertedId.toString() : null
     },
+
+    async findComment(commentId: string) {
+        const comment = await commentCollection.findOne({_id: new ObjectId(commentId)})
+        return comment ? comment : null
+    },
+
+    async updateComment(commentId: string, content: CommentInputModel): Promise<boolean> {
+        debugger
+        const result = await commentCollection.updateOne({_id: new ObjectId(commentId)},
+            {
+                $set: {
+                    content: content.toString(),
+                }
+            })
+        return result.matchedCount === 1
+    }
 
 }
