376ea6f238ead30b12224a443d341f9ee5cb0add
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-25 20:06:22 +0500

    Update existing comment by id with InputModel

--- src/db/mongo-db.ts
+++ src/db/mongo-db.ts
@@ -3,7 +3,7 @@ import {appConfig} from "../appConfig";
 import {BlogsDbType} from "../features/blogs/blogsModel";
 import {PostsDbType} from "../features/posts/post-type";
 import {UsersDbModel} from "../features/users/types/usersDbModel";
-import {CommentDBType} from "../features/comments/commentModel";
+import {CommentDbModel} from "../features/comments/commentModel";
 
 export const client: MongoClient = new MongoClient(appConfig.MONGO_URL)
 export const db : Db = client.db(appConfig.DB_NAME)
@@ -11,7 +11,7 @@ export const db : Db = client.db(appConfig.DB_NAME)
 export const blogCollection: Collection<BlogsDbType> = db.collection<BlogsDbType>(appConfig.BLOG_COLLECTION_NAME)
 export const postCollection: Collection<PostsDbType> = db.collection<PostsDbType>(appConfig.POST_COLLECTION_NAME)
 export const userCollection: Collection<UsersDbModel> = db.collection<UsersDbModel>(appConfig.USER_COLLECTION_NAME)
-export const commentCollection: Collection<CommentDBType> = db.collection<CommentDBType>(appConfig.COMMENTS_COLLECTION_NAME)
+export const commentCollection: Collection<CommentDbModel> = db.collection<CommentDbModel>(appConfig.COMMENTS_COLLECTION_NAME)
 export const connectToDB = async () => {
     try {
         await client.connect()
