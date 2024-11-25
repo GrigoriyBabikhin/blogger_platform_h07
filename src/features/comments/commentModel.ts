cbb2df7d6fe445a07176c5da03605b163be11098
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-25 20:06:15 +0500

    Update existing comment by id with InputModel

--- src/features/comments/commentModel.ts
+++ src/features/comments/commentModel.ts
@@ -2,22 +2,25 @@ export type CommentInputModel = {
     content: string;
 }
 
+export type CommentIdModel = {
+    commentId: string;
+}
+
 export type CommentViewModel = {
     id: string;
     content: string;
-    commentatorInfo: CommentatorInfo;
+    commentatorInfo: CommentatorInfoModel;
     createdAt: string;
-
 }
 
-export type CommentatorInfo = {
+export type CommentatorInfoModel = {
     userId: string
     userLogin: string
 }
 
-export type CommentDBType = {
+export type CommentDbModel = {
     postId: string;
     content: string;
-    commentatorInfo: CommentatorInfo;
+    commentatorInfo: CommentatorInfoModel;
     createdAt: string;
 }
