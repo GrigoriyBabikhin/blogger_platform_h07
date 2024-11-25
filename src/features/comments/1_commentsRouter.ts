41025bde793d2d2a2fdddd8d60c9c9add3673e6d
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-11-25 20:06:05 +0500

    Update existing comment by id with InputModel

--- src/features/comments/1_commentsRouter.ts
+++ src/features/comments/1_commentsRouter.ts
@@ -1,3 +1,10 @@
 import {Router} from "express";
+import {accessTokenGuard} from "../auth/guards/accessTokenGuard";
+import {commentsController} from "./2_commentsController";
+import {commentsInputValidation} from "./commentsValidation";
 
 export const commentsRouter = Router({})
+
+commentsRouter.get('/:commentId', commentsController.getComment)
+commentsRouter.put('/:commentId',accessTokenGuard, ...commentsInputValidation(), commentsController.updateComment)
+commentsRouter.delete('/:commentId',accessTokenGuard)
